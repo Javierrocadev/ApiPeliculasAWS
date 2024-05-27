96e1cce4b03dad2e2e32d3bbd60a88aaaf2a790e
Javier Roca <javierroca.dev@gmail.com>
2024-05-27 13:28:12 +0200

    Add project files.

---
+++ ApiPeliculasAWS/Models/Pelicula.cs
@@ -0,0 +1,52 @@
+﻿using ApiPeliculasAWS.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasAWS.Models
+{
+    [Table("pelismysql")]
+    public class Pelicula
+    {
+        [Key]
+        [Column("IdPelicula")]
+        public int IdPelicula { get; set; }
+
+        [Column("Genero")]
+        public string Genero { get; set; }
+
+        [Column("Titulo")]
+        public string Titulo { get; set; }
+
+        [Column("Nacionalidad")]
+        public string Nacionalidad { get; set; }
+
+        [Column("Argumento")]
+        public string Argumento { get; set; }
+
+        [Column("Actores")]
+        public string Actores { get; set; }
+
+        [Column("Duracion")]
+        public int Duracion { get; set; }
+
+        [Column("Precio")]
+        public int Precio { get; set; }
+
+        [Column("Youtube")]
+        public string Youtube { get; set; }
+    }
+}
+//IdPelicula int NOT NULL PRIMARY KEY,
+//    Genero nvarchar(50) NULL,
+//    Titulo nvarchar(255) NULL,
+//    Nacionalidad nvarchar(50) NULL,
+//    Argumento nvarchar(5000) NULL,
+//    Actores nvarchar(5000) NOT NULL,
+//    Duracion int NULL,
+//    Precio int NULL,
+//    Youtube nvarchar(255) NULL
---
+++ ApiPeliculasAWS/Repositories/RepositoryPeliculas.cs
@@ -0,0 +1,48 @@
+﻿using ApiPeliculasAWS.Data;
+using ApiPeliculasAWS.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasAWS.Repositories
+{
+    public class RepositoryPeliculas
+    {
+        private PeliculasContext context;
+
+        public RepositoryPeliculas(PeliculasContext context)
+        {
+            this.context = context;
+        }
+
+
+
+
+
+        public async Task<List<Pelicula>> GetPeliculasAsync()
+        {
+            return await this.context.Peliculas.ToListAsync();
+        }
+
+
+        public async Task<Pelicula> FindPeliculaAsync(int idpelicula)
+        {
+            return await this.context.Peliculas.FirstOrDefaultAsync(x => x.IdPelicula == idpelicula);
+        }
+
+
+        public async Task<List<Pelicula>> GetPeliculasByActor(string actor)
+        {
+            return await this.context.Peliculas
+                .Where(p => p.Actores.Contains(actor))
+                .ToListAsync();
+        }
+
+
+    }
+}
